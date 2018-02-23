0c3115176ad50d698f7f751ee15dc5e4a6fa4908
X-SLAYER <xslayer404@yahoo.com>
2018-02-23 21:56:42 +0100

    Add files via upload

---
+++ Youtube Subscribers Livecount/Youtube Subscribers Livecount/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lname = New System.Windows.Forms.Label()
+        Me.lcount = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lname
+        '
+        Me.lname.Font = New System.Drawing.Font("Segoe UI", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lname.Location = New System.Drawing.Point(-1, 25)
+        Me.lname.Name = "lname"
+        Me.lname.Size = New System.Drawing.Size(801, 64)
+        Me.lname.TabIndex = 0
+        Me.lname.Text = "Channel ID"
+        Me.lname.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lcount
+        '
+        Me.lcount.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lcount.Location = New System.Drawing.Point(-1, 121)
+        Me.lcount.Name = "lcount"
+        Me.lcount.Size = New System.Drawing.Size(801, 91)
+        Me.lcount.TabIndex = 1
+        Me.lcount.Text = "0"
+        Me.lcount.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(254, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(795, 274)
+        Me.Controls.Add(Me.lcount)
+        Me.Controls.Add(Me.lname)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Youtube Subscribers Livecounts"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lname As System.Windows.Forms.Label
+    Friend WithEvents lcount As System.Windows.Forms.Label
+
+End Class
---
+++ Youtube Subscribers Livecount/Youtube Subscribers Livecount/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Text
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Imports System.Net
+Imports System.Threading.Tasks
+
+Public Class Form1
+
+    Dim ID As String
+
+    Public Sub New()
+        InitializeComponent()
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Public Sub Livecounts()
+        lname.Text = Regex.Match(New Net.WebClient().DownloadString("https://www.youtube.com/channel/" & ID), "<meta name=""title"" content=""(.*?)"">").Groups(1).Value
+Live:
+        Using x As New WebClient
+            x.Encoding = Encoding.UTF8
+            lcount.Text = FormatNumber(Regex.Match(x.DownloadString("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" & ID & "&fields=items(statistics(commentCount%2CsubscriberCount%2CvideoCount%2CviewCount))&key=AIzaSyA1n4M-fo2Y5NHUj0RsvXEAis3H6_lIjRg"), """subscriberCount"": ""(.*?)"",").Groups(1).Value, 0, TriState.True)
+        End Using
+        GoTo Live
+
+    End Sub
+
+    Private Sub lname_Click(sender As Object, e As EventArgs) Handles lname.Click
+        ID = InputBox("Insert Channel ID", "Insert Channel ID", "")
+        If ID = Nothing Then
+            Exit Sub
+        End If
+        Dim SLAYER As Task = Task.Factory.StartNew(AddressOf Livecounts)
+    End Sub
+End Class
